c841b0d0dce4322b8980ce2a8b4f83e9b6c510f0
RHns <103296021+ImRHns@users.noreply.github.com>
2022-05-17 18:30:38 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -2,7 +2,7 @@ import { tiktokdl } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
     const { author: { nickname }, video, description } = await tiktokdl(args[0])
-    const url = video.no_watermark || video.with_watermark2 || video.no_watermark_raw
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
     if (!url) throw 'Can\'t download video!'
     conn.sendFile(m.chat, url, 'tiktok.mp4', `
 🔗 *Url:* ${url}
