d67c14b5886b14f56500a0cb31daf9e7260f05c8
Genjieeehh <106309303+Genjieeehh@users.noreply.github.com>
2022-05-26 18:06:17 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler =  m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085713964963]
-│ • Telkomsel [081319944687]
+│ • Indosat Ooredoo [085752423288]
+│ • Axis [083181913421]
 ╰────
 
 ╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/BochilGaming
-│ • Gopay [085713964963]
+│ • Dana/Shopeepay [085752423288]
+│ • A/N Sadera
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
