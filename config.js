2c544f80bba12b0c31f291b6d3da633e0aa242af
Dimas-Tokisaki <115859821+Dimas-Tokisaki@users.noreply.github.com>
2022-11-19 21:40:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281390658325'],
-  ['6282256115584'],
-  ['6281319944687', 'BG', true]
+  ['6283169648444'],
+  ['6283169648444'],
+  ['6289647022414', 'DimasKiyotaka', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -30,8 +30,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot'
+global.packname = 'wa.me/6283169648444'
+global.author = 'DimasKiyotaka'
 
 global.multiplier = 69 // The higher, The harder levelup
 
